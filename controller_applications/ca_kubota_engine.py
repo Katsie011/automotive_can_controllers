a5185390b45db204d45e701bd12876c3f0f31ebf
Michael Katsoulis <michaelkatsoulis@gmail.com>
2025-06-18 11:35:57 +0200

    Move hard coded numbers for conversions into constants

--- controller_applications/ca_kubota_engine.py
+++ controller_applications/ca_kubota_engine.py
@@ -15,29 +15,33 @@ import j1939
 ## For python 3.12:
 type CYCLIC_MESSAGE_TYPE = tuple[function, int]
 
-# CONSTANTS
-BITS_PER_LITRE_FUEL = 0.05  # 0.05 L/h per bit
-
-
-"""0000: start not requested
-0010: starter active (gear engaged)
-0100: starter inhibited due to engine already running
-1100: starter inhibited
-Other status are not supported
-"""
-ENGINE_STARTER_MODE = {
-    0b0000: "START_NOT_REQUESTED",
-    0b0010: "STARTER ACTIVE",
-    0b0100: "START INHIBITED - ENGINE ALREADY RUNNING",
-    0b1100: "START INHIBITED ",
-}
-
 
 class Kubota_D902k_CA(j1939.ControllerApplication):
     """
     Controller Application for reading and writing to the Kubota D902k engine
 
     """
+
+    # CONSTANTS
+    BITS_PER_LITRE_FUEL = 0.05  # 0.05 L/h per bit
+    BITS_PER_RPM_ENGINE_SPEED = 0.125  # 0.125 rpm/bit
+    BITS_PER_PERCENT_THROTTLE = 0.4  # 0.4 %/bit
+    BITS_PER_VOLT_BATTERY_POTENTIAL = 0.05  #  0.05V/bit
+    BITS_PER_KPA_BAROMETRIC_PRESSURE = 0.5  # 0.5 kPa/bit
+    BITS_PER_LITRE_TOTAL_FUEL_USED = 0.5  # 0.5 L/bit
+
+    """0000: start not requested
+    0010: starter active (gear engaged)
+    0100: starter inhibited due to engine already running
+    1100: starter inhibited
+    Other status are not supported
+    """
+    ENGINE_STARTER_MODE = {
+        0b0000: "START_NOT_REQUESTED",
+        0b0010: "STARTER ACTIVE",
+        0b0100: "START INHIBITED - ENGINE ALREADY RUNNING",
+        0b1100: "START INHIBITED ",
+    }
 
     def __init__(self, name, device_address_preferred=None) -> None:
         self.decoders = {
@@ -89,7 +93,7 @@ class Kubota_D902k_CA(j1939.ControllerApplication):
 
         # Try to decode the message
         if pgn in self.decoders.keys():
-            result = kubota.decode(pgn, data)
+            result = self.decode(pgn, data)
             # Print the decoded information if successful
             if type(result) == dict:
                 if "error" not in result:
@@ -108,7 +112,9 @@ class Kubota_D902k_CA(j1939.ControllerApplication):
         """EEC1 - Engine Speed, Torque, Starter Mode, etc."""
         demand_torque = data[1] - 125
         actual_torque = data[2] - 125
-        engine_speed = self._bits_to_engine_speed(struct.unpack_from("<H", data, 3)[0])
+        engine_speed = self._engine_speed_bits_to_rpm(
+            struct.unpack_from("<H", data, 3)[0]
+        )
         tsc1_src_address = data[5]
         # starter_mode = (
         #     data[6] & 0xF0
@@ -120,7 +126,9 @@ class Kubota_D902k_CA(j1939.ControllerApplication):
             "Driver's Demand Torque (%)": demand_torque,
             "Actual Engine Torque (%)": actual_torque,
             "Starter Mode": starter_mode,
-            "Starter Message": ENGINE_STARTER_MODE.get(starter_mode, "STATUS UNKNOWN"),
+            "Starter Message": self.ENGINE_STARTER_MODE.get(
+                starter_mode, "STATUS UNKNOWN"
+            ),
             "TSC1 Source Address": tsc1_src_address,
         }
 
@@ -187,12 +195,9 @@ class Kubota_D902k_CA(j1939.ControllerApplication):
         """LFE - Fuel Rate, Throttle Position"""
 
         # 2 bytes, position 1-2,  0 to 3212.75 L/h, 0.05 L/h per bit, 0 offset
-        fuel_rate = (
-            struct.unpack_from("<H", data, 0)[0] * 0.05
-        )  # Check that this decodes 2 bytes
+        fuel_rate = self._fuel_rate_bits_to_litres(struct.unpack_from("<H", data, 0)[0])
 
-        # Starts position 7, 1 byte,  0 to 100%, 0.4 %/bit, 0 offset
-        throttle = data[6] * 0.4
+        throttle = self._throttle_bits_to_percent(data[6])
         return {
             "PGN": 65266,
             "Fuel Rate (L/h)": fuel_rate,
@@ -202,7 +207,9 @@ class Kubota_D902k_CA(j1939.ControllerApplication):
     def decode_65271(self, data):
         """VEP1 - Battery Potential"""
         # position 5-6, 2 bytes,  0 to 3212.75 V, 0.05 V/bit, 0 offset, rate 1000ms
-        voltage = struct.unpack_from("<H", data, 4)[0] * 0.05
+        voltage = (
+            struct.unpack_from("<H", data, 4)[0] * self.BITS_PER_VOLT_BATTERY_POTENTIAL
+        )
         return {
             "PGN": 65271,
             "Battery Voltage (V)": voltage,
@@ -211,7 +218,7 @@ class Kubota_D902k_CA(j1939.ControllerApplication):
     def decode_65269(self, data):
         """AMB - Barometric Pressure"""
         #  0 to 125 kPa, 0.5 kPa/bit, 0 offset
-        pressure = data[0] * 0.5
+        pressure = data[0] * self.BITS_PER_KPA_BAROMETRIC_PRESSURE
         return {
             "PGN": 65269,
             "Barometric Pressure (kPa)": pressure,
@@ -220,7 +227,10 @@ class Kubota_D902k_CA(j1939.ControllerApplication):
     def decode_65257(self, data):
         """LFC - Total Fuel Used"""
         #  0 to 2105540607.5 L, 0.5 L/bit, 0 offset
-        fuel_total = struct.unpack_from("<I", data, 4)[0] * 0.5
+
+        fuel_total = (
+            struct.unpack_from("<I", data, 4)[0] * self.BITS_PER_LITRE_TOTAL_FUEL_USED
+        )
         return {
             "PGN": 65257,
             "Total Fuel Used (L)": fuel_total,
@@ -236,13 +246,22 @@ class Kubota_D902k_CA(j1939.ControllerApplication):
             "Shutdown Active": shutdown,
         }
 
-    def _engine_speed_to_bits(self, speed: float) -> int:
+    def _engine_speed_rpm_to_bits(self, rpm: float) -> int:
         """0 to 8031.875 rpm, 0.125 rpm/bit, 0 offset"""
-        return int(speed / 0.125)
+        if rpm < 0 or rpm > 8031.875:
+            raise ValueError(
+                f"Engine speed must be between 0 and 8031.875 RPM, got {rpm}"
+            )
+        return int(rpm / self.BITS_PER_RPM_ENGINE_SPEED)
 
-    def _bits_to_engine_speed(self, bits: int) -> float:
+    def _engine_speed_bits_to_rpm(self, bits: int) -> float:
         """0 to 8031.875 rpm, 0.125 rpm/bit, 0 offset"""
-        return bits * 0.125
+        rpm = bits * self.BITS_PER_RPM_ENGINE_SPEED
+        if rpm < 0 or rpm > 8031.875:
+            raise ValueError(
+                f"Engine speed must be between 0 and 8031.875 RPM, got the equivalent of {rpm}"
+            )
+        return rpm
 
     def _fuel_rate_litres_to_bits(self, litres: float) -> int:
         """0 to 3212.75 L/h, 0.05 L/h per bit, 0 offset"""
@@ -250,19 +269,34 @@ class Kubota_D902k_CA(j1939.ControllerApplication):
             raise ValueError(
                 f"Fuel rate must be between 0 and 3212.75 L/h, got {litres}"
             )
-
-        return int(litres / BITS_PER_LITRE_FUEL)
-        return bits * 0.125
+        return int(litres / self.BITS_PER_LITRE_FUEL)
 
     def _fuel_rate_bits_to_litres(self, bits: int) -> float:
         """0 to 3212.75 L/h, 0.05 L/h per bit, 0 offset"""
-        litres = bits * 0.05
+        litres = bits * self.BITS_PER_LITRE_FUEL
         if litres < 0 or litres > 3212.75:
             raise ValueError(
                 f"Fuel rate must be between 0 and 3212.75 L/h, got the equivalent of {litres}"
             )
-
         return litres
+
+    def _throttle_bits_to_percent(self, bits: int) -> float:
+        """Starts position 7, 1 byte,  0 to 100%, 0.4 %/bit, 0 offset"""
+        percent = bits * self.BITS_PER_PERCENT_THROTTLE
+        if percent < 0 or percent > 100:
+            raise ValueError(
+                f"Throttle position must be between 0 and 100%, got the equivalent of {percent}%"
+            )
+        return percent
+
+    def _throttle_percent_to_bits(self, percent: float) -> int:
+        """Starts position 7, 1 byte,  0 to 100%, 0.4 %/bit, 0 offset"""
+        if percent < 0 or percent > 100:
+            raise ValueError(
+                f"Throttle position must be between 0 and 100%, got the equivalent of {percent}%"
+            )
+        bits = int(percent / self.BITS_PER_PERCENT_THROTTLE)
+        return bits
 
 
 # struct.unpack assumes little endian and uses <H for unsigned short, <I for unsigned int
